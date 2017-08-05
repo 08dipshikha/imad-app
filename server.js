40fd5e79b43f7067321ca162666ab089ec74429f
08dipshikha <0802dipshikhadas@gmail.com>
2017-08-05 12:05:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/artone',function(req,res){
+app.get('/article-one',function(req,res){
  res.send('Article one is requested and will be served here');   
 });
 
-app.get('/arttwo',function(req,res){
+app.get('/article-two',function(req,res){
  res.send('Article one is requested and will be served here');   
 });
 
-app.get('/artthree',function(req,res){
+app.get('/article-three',function(req,res){
  res.send('Article one is requested and will be served here');   
 });
 
